0bb6e0c3dc12ef08d9db9e8ae40f4c97076d7ad2
Martin Nilsson <martin.jr.nilsson@gmail.com>
2020-01-15 15:38:49 +0100

    add disco

--- colorchanger.js
+++ colorchanger.js
@@ -1,27 +1,38 @@
 var express = require('express');
 var app = express();
 
-app.use('/static', express.static('content'));
-
-app.get('/', function (req, res) {
-  res.redirect('/static/index.html');
-})
-
-var startTime = new Date().getTime();
+var startTime = Math.floor(new Date().getTime() / 1000);
 var intervalSeconds = 2;
 var durationMinutes = 60;
 var discolights = new Array();
+
+app.use('/static', express.static('content'));
+
+app.get('/', function (req, res) {
+	res.redirect('/static/index.html');
+  })
+
+app.get('/lights', function (req, res) {
+	res.json(discolights);
+})	
 
 console.log('generating disco lights sequences');
 
 var i = 0;
 while(true) {
-	if (i * intervalSeconds * 60 > durationMinutes) {
+	if (i * intervalSeconds > durationMinutes * 60) {
 		break;
 	}
 	
-	discolights.push({startTime + })
+	discolights.push({time: startTime + i, color: randomColor()});
+	i += intervalSeconds;
 }
 
-app.listen(3000);
+console.log(JSON.stringify(discolights));
+
+app.listen(3000, '0.0.0.0');
 console.log('listening on port 3000');
+
+function randomColor() {
+	return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
--- content/index.js
+++ content/index.js
@@ -1,31 +1,74 @@
 (function(document) {
     'use strict';
 
-    function randomColor() {
-        return '#'+Math.floor(Math.random()*16777215).toString(16);
-    }
+	var index = 0;
+	var lights;
+	
+	fetch('/lights').then(function(res) {
+		var el = document.querySelector('h1');
+		el.textContent = "fetched..";
+		return res.json();
+	}).then(function(result) {
+		lights = result;
+	}); 
+    // function randomColor() {
+    //     return '#'+Math.floor(Math.random()*16777215).toString(16);
+	// }
+	
+	setInterval(function() {
+		var time = Math.floor(new Date().getTime() / 1000);
 
-    function setContent(event) {
-        var e = event || window.event;
-        var charCode = e.which || e.keyCode;
-        var char = String.fromCharCode(charCode);
-        var color = randomColor();
-        var background = randomColor();
-        var el = document.querySelector('h1');
-        var content = el.textContent;
-        if (content.length > 5) {
-            content = content.slice(1, content.length - 5);
-            content += char;
-        }
-        else {
-            content += char;
-        }
-        el.textContent = content;
-        el.style.color = color;
+		console.log("trying to change color");
+		
+		if (lights == null) {
+			console.log("lights data is empty");
+			return;
+		}
+
+		for(var i = index; i < lights.length; i++) {
+			var el = lights[i];
+			if (el.time < time) {
+				console.log(`time too low ${el.time} < ${time}`);
+				continue;
+			}
+
+			if (index === i) {
+				console.log("color and time already set to this");
+				return;
+			}
+
+			try {
+				console.log("changing");
+				setContent(el.color);
+			}
+			finally {
+				return;
+			}
+		}
+
+	}, 250);
+
+    function setContent(color) {
+        // var e = event || window.event;
+        // var charCode = e.which || e.keyCode;
+        // var char = String.fromCharCode(charCode);
+        //var color = randomColor();
+        var background = color;
+        // var el = document.querySelector('h1');
+        // var content = el.textContent;
+        // if (content.length > 5) {
+        //     content = content.slice(1, content.length - 5);
+        //     content += char;
+        // }
+        // else {
+        //     content += char;
+        // }
+        // el.textContent = content;
+        // el.style.color = color;
         document.body.style.background = background;
-        document.querySelector('#color').textContent = color;
+        //document.querySelector('#color').textContent = color;
         document.querySelector('#background').textContent = background;
     }
 
-    document.onkeypress = setContent;
+    //document.onkeypress = setContent;
 }(document));
