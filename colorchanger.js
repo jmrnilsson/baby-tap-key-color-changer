54d104368073cbcf71081a28eb295bf1e844044c
Martin Nilsson <martin.jr.nilsson@gmail.com>
2020-01-15 14:42:16 +0100

    add ligghts

--- colorchanger.js
+++ colorchanger.js
@@ -7,5 +7,21 @@ app.get('/', function (req, res) {
   res.redirect('/static/index.html');
 })
 
+var startTime = new Date().getTime();
+var intervalSeconds = 2;
+var durationMinutes = 60;
+var discolights = new Array();
+
+console.log('generating disco lights sequences');
+
+var i = 0;
+while(true) {
+	if (i * intervalSeconds * 60 > durationMinutes) {
+		break;
+	}
+	
+	discolights.push({startTime + })
+}
+
 app.listen(3000);
 console.log('listening on port 3000');
